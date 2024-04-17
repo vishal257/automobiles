76d5295d1539b462ce527efc7b4a2468cd5f195f
Vishal <vishal2573003@gmail.com>
2024-04-17 22:53:32 +0530

    optimised images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import imageUrl from "../../public/hero-banner.jpg";
+
 export default function Home() {
   return (
     <div>
-      pola
+      <div className="relative h-80">
+        <Image
+          src={imageUrl}
+          alt="bgImg"
+          fill
+          placeholder="blur"
+          style={{ objectFit: "cover" }}
+          quality={100}
+        ></Image>
+      </div>
     </div>
   );
 }
