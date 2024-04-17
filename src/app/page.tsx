566bf883523ba6665641c22b6bf78d6001eef01a
Vishal <vishal2573003@gmail.com>
2024-04-17 23:22:15 +0530

    responsive UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,13 @@ export default function Home() {
           quality={100}
         ></Image>
       </div>
-      <div className="uppercase grid justify-items-center gap-2 absolute place-self-center text-stone-100">
-        <div className="font-black text-5xl">Satyam Auto Repair Shop</div>
-        <div className="font-extrabold text-4xl">
+      <div className="uppercase grid justify-items-center gap-2 absolute place-self-center text-stone-100 p-4">
+        <div className="font-black md:text-5xl sm:text-4xl sxm:text-xl text-lg">Satyam Auto Repair Shop</div>
+        <div className="font-extrabold md:text-4xl sm:text-3xl text-lg text-center">
           503-221-2411 • MONDAY - FRIDAY 7:00AM to 5:30PM
         </div>
         <div className="pt-4">
-          <button className="bg-red-800 text-white font-bold text-2xl py-2 px-8 hover:bg-stone-500 rounded-md">
+          <button className="bg-red-800 text-white font-bold md:text-2xl sm:text-xl text-md py-2 px-8 hover:bg-stone-500 rounded-md">
             <Link href="/appointment">
               Schedule an Appointment
             </Link>
