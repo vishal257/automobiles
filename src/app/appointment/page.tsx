7b29d143863b7ea6b478da30ffe655675f9cefac
Vishal <vishal2573003@gmail.com>
2024-04-25 21:09:49 +0530

    added form

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -1,7 +1,24 @@
-export default function Page(){
-    return(
+export default function Page() {
+  return (
+    <div>
+      <div>
+          <div>Select Your Service</div>
+      </div>
+      <div>
+        <div>Popular Services</div>
         <div>
-            <h1>Appointment</h1>
+            <form>
+                <input type="checkbox" name="Brakes">Brakes</input>
+                <input type="checkbox" name="Brakes">Oil Change</input>
+                <input type="checkbox" name="Brakes">Vehicle Inspection</input>
+                <input type="checkbox" name="Brakes">Tires</input>
+                <input type="checkbox" name="Brakes">Battery</input>
+                <input type="checkbox" name="Brakes">Engine & Transmission</input>
+                <input type="checkbox" name="Brakes">Heat or A/C</input>
+                <input type="checkbox" name="Brakes">Schedule Maintenance</input>
+            </form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
