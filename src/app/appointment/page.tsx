f891b8426b832a45d0422e9bcf3754fa43e87b76
Vishal <vishal2573003@gmail.com>
2024-04-25 21:22:41 +0530

    modified form

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -1,17 +1,17 @@
 export default function Page() {
   return (
-    <div>
+    <div className="text-center">
       <div>
-          <div>Select Your Service</div>
+          <div className="font-black text-4xl">Select Your Service</div>
       </div>
       <div>
-        <div>Popular Services</div>
-        <div>
-            <form>
+        <div className="font-extrabold text-3xl">Popular Services</div>
+        <div className="">
+            <form className="grid md:grid-cols-4 grid-cols-2">
                 <label htmlFor="Brakes">Brakes</label>
                 <input type="checkbox" name="Brakes"></input>
-                <label htmlFor="Oil Change">Brakes</label>
-                <input type="checkbox" name="Oil Change"></input>
+                <label htmlFor="OilChange">Oil Change</label>
+                <input type="checkbox" name="OilChange"></input>
                 <label htmlFor="VehicleInspection">Vehicle Inspection</label>
                 <input type="checkbox" name="VehicleInspection"></input>
                 <label htmlFor="Tires">Tires</label>
