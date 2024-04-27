a19c8b90fcc33c0d962960dc467b54044a708706
Vishal <vishal2573003@gmail.com>
2024-04-27 20:07:02 +0300

    refactored form

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -1,12 +1,19 @@
-function FormData(){
+interface FormDataProps{
+  label:string,
+  labelName:string,
+  func:(event:React.MouseEvent<HTMLDivElement>) => void
+}
+
+
+function FormData({label, labelName, func}:FormDataProps){
   return(
-    <div className="bg-slate-200 p-5 rounded-lg grid grid-cols-2 justify-between">
-    <label htmlFor="Brakes" className="justify-self-start">
-      Brakes
+    <div className="bg-slate-200 p-5 rounded-lg grid grid-cols-2 justify-between cursor-pointer" onClick={func}>
+    <label htmlFor={labelName} className="justify-self-start">
+      {label}
     </label>
     <input
       type="checkbox"
-      name="Brakes"
+      name={labelName}
       className="justify-self-end size-6"
     ></input>
     </div>
@@ -15,6 +22,9 @@ function FormData(){
 
 
 export default function Page() {
+  function formChecker(){
+    return 0;
+  }
   return (
     <div className="text-center py-8 w-3/4 mx-auto">
       <div>
@@ -24,93 +34,14 @@ export default function Page() {
         <div className="font-extrabold text-3xl py-5">Popular Services</div>
         <div className="font-medium text-xl text-left">
           <form className="grid md:grid-cols-2 grid-cols-1 gap-4 w-8/12 mx-auto">
-            <div className="bg-slate-200 p-5 rounded-lg grid grid-cols-2 justify-between">
-              <label htmlFor="Brakes" className="justify-self-start">
-                Brakes
-              </label>
-              <input
-                type="checkbox"
-                name="Brakes"
-                className="justify-self-end size-6"
-              ></input>
-            </div>
-            <FormData/>
-            <div className="bg-slate-200 p-5 rounded-lg grid grid-cols-2 justify-between">
-              <label htmlFor="OilChange" className="justify-self-start">
-                Oil Change
-              </label>
-              <input
-                type="checkbox"
-                name="OilChange"
-                className="justify-self-end size-6"
-              ></input>
-            </div>
-            <div className="bg-slate-200 p-5 rounded-lg grid grid-cols-2 justify-between">
-              <label htmlFor="VehicleInspection" className="justify-self-start lg:whitespace-nowrap">
-                Vehicle Inspection
-              </label>
-              <input
-                type="checkbox"
-                name="VehicleInspection"
-                className="justify-self-end size-6"
-              ></input>
-            </div>
-            <div className="bg-slate-200 p-5 rounded-lg grid grid-cols-2 justify-between">
-              <label htmlFor="Tires" className="justify-self-start">
-                Tires
-              </label>
-              <input
-                type="checkbox"
-                name="Tires"
-                className="justify-self-end size-6"
-              ></input>
-            </div>
-            <div className="bg-slate-200 p-5 rounded-lg grid grid-cols-2 justify-between">
-              <label htmlFor="Battery" className="justify-self-start">
-                Battery
-              </label>
-              <input
-                type="checkbox"
-                name="Battery"
-                className="justify-self-end size-6"
-              ></input>
-            </div>
-            <div className="bg-slate-200 p-5 rounded-lg grid grid-cols-2 justify-between">
-              <label
-                htmlFor="EngineTransmission"
-                className="justify-self-start xl:whitespace-nowrap"
-              >
-                Engine & Transmission
-              </label>
-              <input
-                type="checkbox"
-                name="EngineTransmission"
-                className="justify-self-end size-6"
-              ></input>
-            </div>
-            <div className="bg-slate-200 p-5 rounded-lg grid grid-cols-2 justify-between">
-              <label htmlFor="HeatAC" className="justify-self-start">
-                Heat or A/C
-              </label>
-              <input
-                type="checkbox"
-                name="HeatAC"
-                className="justify-self-end size-6"
-              ></input>
-            </div>
-            <div className="bg-slate-200 p-5 rounded-lg grid grid-cols-2 justify-between">
-              <label
-                htmlFor="ScheduleMaintenance"
-                className="justify-self-start xl:whitespace-nowrap"
-              >
-                Schedule Maintenance
-              </label>
-              <input
-                type="checkbox"
-                name="ScheduleMaintenance"
-                className="justify-self-end size-6"
-              ></input>
-            </div>
+            <FormData label="Brakes" labelName="Brakes" func={formChecker}/>
+            <FormData label="Oil Change" labelName="OilChange" func={formChecker}/>
+            <FormData label="Vehicle Inspection" labelName="VehicleInspection" func={formChecker}/>
+            <FormData label="Tires" labelName="Tires" func={formChecker}/>
+            <FormData label="Battery" labelName="Battery" func={formChecker}/>
+            <FormData label="Engine & Transmission" labelName="EngineTransmission" func={formChecker}/>
+            <FormData label="Heat or A/C" labelName="HeatAC" func={formChecker}/>
+            <FormData label="Schedule Maintenance" labelName="ScheduleMaintenance" func={formChecker}/>
           </form>
         </div>
         <div>
