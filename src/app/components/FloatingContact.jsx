1615292f2419b934b6b9132af3894c89737a1a6d
Vishal <vishal2573003@gmail.com>
2024-08-05 15:30:40 +0530

    added whatsapp config

--- src/app/components/FloatingContact.jsx
+++ src/app/components/FloatingContact.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const FloatingContact = () => {
   return (      
     <div className=" fixed right-4 bottom-6 bg-slate-200 rounded-lg z-50">
-    <a href="https://wa.me/+919592348990" target="_blank" className="flex md:gap-4 gap-2 items-center md:px-4 md:py-2">
+    <a href="https://wa.me/+919592348990" target="_blank" className="flex md:gap-4 gap-2 items-center md:px-4 md:py-2 px-2 py-1">
     <div className="">
           <Image
             src={whatsappImg}
